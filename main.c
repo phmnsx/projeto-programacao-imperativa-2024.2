75a1a21154dff404f65725da3bc78534aaa6fd40
phmnsx <lynnwastinghertime@gmail.com>
2025-03-11 19:34:13 -0300

    Criação de structs e comentarios
    Criação de structs e comentários que fizemos no lab do DCOMP

--- main.c
+++ main.c
@@ -1,10 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv)
+typedef struct 
 {
-	if (argc < 2) printf("oi mundo\n");
-	else printf("oi %s", argv[1]);
-	return 0;
-}
+	int i;
+	int j;
+} point;
 
-//Olá! Esse arquivo foi alterado.
+typedef struct node node;
+struct node
+{
+	point pos;
+	struct node* origin;
+};
+
+typedef struct
+{
+int parede = 0;
+int chao = 0;
+int inimigo = 0;
+int inicio = 0;
+int chegada = 0;
+int andou = 0;
+int morreu = 0;
+int matou = 0;
+int vitoria = 0;
+} tile;
+
+
+int main()
+{
+	//ler modo
+	//abrir arquivo e ler labirinto (saber tamanho?)
+	//tamanho[i][j]
+	
+	//determinar posição inicial, em (i j) e criar node
+	//determinar posição final
+	//ler os espaços que tem em volta da primeira node;
+	
+	//caso espaço seja andável, criar node nele;
+	//repete para todos os nodes
+	
+	//caso posição do node seja igual à posição final, considere node "completo"
+	//array com nodes completas?
+	
+	//caso len(array) = 0, considere inimigos como não sendo paredes
+	//caso len(array) = 0 de novo, não há solução :(
+	
+	//conte quantos inimigos tem nos caminhos e escolha os que tem menos (menor igual ao menor)
+	
+	//conte o tamanho dos caminhos válidos, e escolha o menor
+	//pegue posição de cada node relacionada a node escolhida e substitua por caracteres apropriados
+	
+	//se modo for 1, escolha se perde; mostre na tela
+	//se for modo 2, ignore; mostre na tela
+	
+	//se modo 4, return 0;
+	//se modo 3 salve a solução em um arquivo;
+	
+}
